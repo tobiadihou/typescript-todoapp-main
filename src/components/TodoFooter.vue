2d0fd71584b034b073b9e5fa62e8b66ce1e6cc17
tobiadihou <adihoutobi@gmail.com>
2024-07-19 15:43:56 +0200

    style filter

--- src/components/TodoFooter.vue
+++ src/components/TodoFooter.vue
@@ -7,13 +7,17 @@
     </span>
     <ul class="filters">
       <li>
-        <router-link to="/">Tous</router-link>
+        <router-link :class="{ selected: route.path === '/' }" to="/">Tous</router-link>
       </li>
       <li>
-        <router-link to="/waiting">En cours</router-link>
+        <router-link :class="{ selected: route.path === 'waiting' }" to="/waiting"
+          >En cours</router-link
+        >
       </li>
       <li>
-        <router-link to="/completed">Terminés</router-link>
+        <router-link :class="{ selected: route.path === 'competed' }" to="/completed"
+          >Terminés</router-link
+        >
       </li>
     </ul>
 
@@ -24,7 +28,9 @@
 <script setup lang="ts">
 import type { Todo } from '@/@types'
 import { computed } from 'vue'
+import { useRoute } from 'vue-router'
 
+const route = useRoute()
 const props = defineProps<{
   todos: Todo[]
 }>()
