8d989d9074d1d7f5be7cad270f28915ca7d1a56e
tobiadihou <adihoutobi@gmail.com>
2024-07-20 13:21:55 +0200

    user login

--- src/configs/firebase.ts
+++ src/configs/firebase.ts
@@ -1,18 +1,23 @@
-import  {initializeApp,type FirebaseAppSettings, type FirebaseOptions } from "firebase/app";
+import  {initializeApp, type FirebaseOptions } from "firebase/app";
 import { getAuth } from "firebase/auth";
 
 const firebaseConfig: FirebaseOptions  = {
   
-    apiKey: import.meta.VITE_API_KEY,
-    authDomain: import.meta.VITE_AUTH_DOMAIN,
-    projectId: import.meta.VITE_PROJECT_IDprojectId ,
-    storageBucket: import.meta.VITE_STORAGE_BUCKET ,
-    messagingSenderId:  import.meta.VITE_MESSAGING_SENDER_ID ,
-    appId:  import.meta.VITE_APP_ID
+  apiKey: import.meta.env.VITE_API_KEY,
+  
+  authDomain: import.meta.env.VITE_AUTH_DOMAIN,
+  
+  projectId: import.meta.env.VITE_PROJECT_ID,
+  
+  storageBucket: import.meta.env.VITE_STORAGE_BUCKET ,
+  
+  messagingSenderId:  import.meta.env.VITE_MESSAGING_SENDER_ID ,
+  
+  appId:  import.meta.env.VITE_APP_ID
   };
 //initialiser firebase
   const app = initializeApp(firebaseConfig);
-  
+
 //initialiser firebase authentification et recuperer les services
   const auth= getAuth(app);
   export {auth};
--- src/main.ts
+++ src/main.ts
@@ -4,9 +4,17 @@ import 'todomvc-app-css/index.css'
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
+//Vuetify
+
+import 'vuetify/styles'
+import {createVuetify} from 'vuetify'
+import * as components from 'vuetify/components'
+
+const vuetify = createVuetify({components})
 
 const app = createApp(App)
-
+app.use(vuetify)
 app.use(router)
+ 
 
 app.mount('#app')
